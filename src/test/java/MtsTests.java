6501828ca0cc090166692546ae8aa0f4a131c4c3
Eprem Chepnyan <156126721+EpChep@users.noreply.github.com>
2024-02-27 23:50:30 +0300

    Commit

--- src/test/java/MtsTests.java
+++ src/test/java/MtsTests.java
@@ -5,42 +5,41 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
+
+
 public class MtsTests {
     private WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        // Установка пути к драйверу Chrome
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver-win64/chromedriver.exe");
-        // Инициализация драйвера
-        driver = new ChromeDriver();
 
-        // Открытие страницы
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
         driver.get("https://www.mts.by");
         try {
             WebElement cookieAgreeButton = driver.findElement(By.cssSelector("#cookie-agree"));
             cookieAgreeButton.click();
         } catch (NoSuchElementException e) {
-            // Ничего не делать, если кнопка не найдена
+
         }
     }
 
    @AfterEach
     public void tearDown() {
-        // Закрытие браузера после каждого теста
+
         driver.quit();
     }
 
     @Test
     public void testBlockTitle() {
-        // Проверка названия блока
+
         WebElement blockTitle = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/h2"));
         assertTrue(blockTitle.isDisplayed());
     }
 
     @Test
     public void testPaymentSystemLogos() {
-        // Проверка наличия шести логотипов платежных систем
+
         for (int i = 1; i <= 6; i++) {
             WebElement logo = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/div[2]/ul/li[" + i + "]/img"));
             assertTrue(logo.isDisplayed());
@@ -49,7 +48,7 @@ public class MtsTests {
 
     @Test
     public void testServiceDetailsLink() {
-        // Проверка работы ссылки "Подробнее о сервисе"
+
         WebElement serviceLink = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/a"));
         serviceLink.click();
         assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", driver.getCurrentUrl());
@@ -57,7 +56,7 @@ public class MtsTests {
 
     @Test
     public void testPaymentProcess() {
-        // Заполнение полей и проверка работы кнопки "Продолжить"
+
         WebElement phoneNumberField = driver.findElement(By.cssSelector("#connection-phone"));
         phoneNumberField.sendKeys("297777777");
 
@@ -75,6 +74,6 @@ public class MtsTests {
 
         assertTrue(popup.isDisplayed());
 
-        // Добавьте здесь проверки после нажатия кнопки "Продолжить"
+
     }
 }
